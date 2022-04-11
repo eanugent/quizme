fae40c77d5ec6f2d875b4e635640f9b1e086d741
eanugent <ean.nugent@gmail.com>
2022-04-10 22:41:40 -0400

    Initial vue 2 app

---
+++ quizme/app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <v-btn
+  elevation="2"
+> Click Me
+</v-btn>
+  </div>
+</template>
+
+<script> 
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
