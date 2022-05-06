0950c676dfc4dc0daecd9785293bb0b41584c1ad
eanugent <ean.nugent@gmail.com>
2022-05-05 22:18:49 -0400

    Use CDN for vuetify css

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -7,7 +7,7 @@
 
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+//import 'vuetify/dist/vuetify.min.css'
 import App from '../app.vue'
 
 Vue.use(Vuetify)
