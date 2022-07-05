06432bac58d76141b1d03eaf8647d038b8bb3bf5
eanugent <ean.nugent@gmail.com>
2022-07-05 15:00:30 -0500

    Pick Subject interface improvements

--- app/javascript/packs/components/pick-subject.vue
+++ app/javascript/packs/components/pick-subject.vue
@@ -2,15 +2,38 @@
     <v-container>
       <v-row>
         <v-col xs="12">
-          <v-card class="my-6 pa-3">
+          <v-card v-if="this.gameStatus == 'intro'" class="pa-3">
             <v-card-title>
               Can you read my mind?
             </v-card-title>
 
             <v-card-text>
-              I'm thinking of a Bible character from the list below. How many questions will it take you to figure out who?
+              <p>
+                I'm thinking of a Bible character from the list below.
+                Can you guess who it is in <strong>{{ maxQuestions }} questions or less</strong>?
+                After you click "READY TO GO!" below, I'll give you 3 questions to choose from. Click "ASK" beside the
+                question that will best help you figure out my character. After I answer that question, I'll give you 3 more to 
+                choose from. When you think you've figured me out, go ahead and take a guess. However, guesses count as questions
+                so use them carefully!
+              </p>
+              <h3>
+                Character List
+              </h3>
+              <h4>(You'll be able to see the list again later)</h4>
+              <div
+                v-for="character in characters"
+                :key="character.name"
+                >
+                {{ character.name }}
+              </div> 
+              
+              <div class="mt-6">
+                <v-btn @click="startGame()">
+                  Ready to Go!
+                </v-btn>
+              </div>
             </v-card-text>
-            <v-card-actions>
+            <!-- <v-card-actions>
               <v-btn
                 color="orange lighten-2"
                 text
@@ -34,13 +57,13 @@
                 </div>   
                 </v-card-text>
               </div>
-            </v-expand-transition>
+            </v-expand-transition> -->
           </v-card>
           
-          <div>
-            <v-card v-if="this.gameStatus == 'in_progress'" class="mb-6 pa-5">
+          <div v-if="this.gameStatus == 'in_progress'" >
+            <v-card class="mb-6 pa-5">
               <v-card-title>
-                Choose a question.
+                Question {{ askedQuestions.length+1 }} / 10
               </v-card-title>
               <v-card-text
                 v-for="question in this.question_options"
@@ -55,9 +78,80 @@
                 Ask
                 </v-btn>
               </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                  @click="showAskedQuestions = !showAskedQuestions"
+                >
+                  Past Questions
+                  <v-icon>{{ showAskedQuestions ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="showAskedQuestions">
+                  <v-divider></v-divider>
+                  <v-card-subtitle>
+                    <span class="green--text">
+                      Yes
+                    </span> |
+                    <span class="red--text">
+                      No
+                    </span> |
+                    <span class="amber--text">
+                      Not sure
+                    </span>
+                </v-card-subtitle>
+                <v-card-text>
+                  <div
+                    v-for="question in this.askedQuestions"
+                    :key="question.id"
+                    :class="`${question.color}--text`"
+                  >
+                    {{ question.text }}?
+                  </div>
+                </v-card-text>
+                </div>
+              </v-expand-transition>
+              
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                  @click="showCharacters = !showCharacters"
+                >
+                  Take a guess
+                  <v-icon>{{ showCharacters ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="showCharacters">
+                  <v-divider></v-divider>
+                <v-card-text>
+                  <div
+                    v-for="character in characters"
+                    :key="character.name"
+                    class="mb-2"
+                    >
+                    <!-- {{ character.name }} -->
+                    <v-btn
+                      color="orange"
+                      :disabled="guessedCharacterIds.includes(character.id)"
+                      x-small
+                      @click="processGuess(character.id, character.name)"
+                    >
+                      {{ character.name }}
+                    </v-btn>
+                  </div>                  
+                </v-card-text>
+                </div>
+              </v-expand-transition> 
             </v-card>
 
-            <v-card class="mb-6">
+            <!-- <v-card class="mb-6">
               <v-card-title>
                 {{ `${askedQuestions.length} Question${askedQuestions.length != 1 ? 's' : ''} asked` }}
               </v-card-title>
@@ -81,9 +175,9 @@
                   {{ question.text }}?
                 </div>
               </v-card-text>
-            </v-card>
+            </v-card> -->
 
-            <v-card v-if="this.gameStatus == 'in_progress'" class="mb-6 pa-5">
+            <!-- <v-card class="mb-6 pa-5">
               <v-card-title>
                 Ready to guess?
               </v-card-title>
@@ -100,7 +194,7 @@
                     style="max-width: 20rem;"
                   ></v-select>
                 </v-card-actions>
-            </v-card>
+            </v-card> -->
           </div>
 
         <v-card v-if="this.message" class="mb-6">
@@ -127,13 +221,16 @@ import axios from "axios";
 
 export default {
   data: () => ({
-    gameStatus: 'choosing_character',
+    maxQuestions: 10,
+    gameStatus: 'intro',
     gameId: -1,
     answerValColors: ['green', 'red', 'amber'],
     message: '',
     question_options: [],
-    characters: [],
     showCharacters: false,
+    characters: [],
+    guessedCharacterIds: [],
+    showAskedQuestions: false,
     askedQuestions: [],
     guess: {}
   }),
@@ -142,7 +239,6 @@ export default {
       .get("/subjects?game_type=Bible Characters")
       .then(response => {
         this.characters = response.data.data;
-        this.startGame();
       })
   },
   methods: {
@@ -183,14 +279,14 @@ export default {
             };
           console.log(questionLog);
           this.askedQuestions.push(questionLog);
-
         });
     },
-    processGuess() {
+    processGuess(subject_id, name) {
+      this.guessedCharacterIds.push(subject_id);
       axios
         .post(`/pick_subject/games/${this.gameId}/process_guess`,
         {
-          subject_id: this.guess.id
+          subject_id: subject_id
         }).then(response => {
           const data = response.data.data;
           console.log(data);
@@ -199,7 +295,7 @@ export default {
 
           const questionLog =
             {
-              text: `Is it ${this.guess.name}`,
+              text: `Is it ${name}`,
               id: -1,
               color: this.answerValColors[data.answer_val-1]
             };
@@ -211,6 +307,7 @@ export default {
     restart() {
       this.message = '';
       this.askedQuestions = [];
+      this.guessedCharacterIds = [];
       this.startGame();
     }
   }
